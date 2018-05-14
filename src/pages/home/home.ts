9d8d0af917101637d4b0dff3a17cf29d47a2b8f9
Roberto Viquez <robertoviquez@gmail.com>
2018-05-14 01:51:25 -0600

    full functional decision making and contact information

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,24 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  result: string = '';
+  disabled: boolean = true;
+  interval: any;
   constructor(public navCtrl: NavController) {
 
+  }
+  decide() {
+    this.interval = setInterval(() => {
+      this.result = this.result === 'yes' ? 'no' : 'yes';
+    }, 450);
+    setTimeout(() => {
+      clearInterval(this.interval);
+      this.result = Math.round(Math.random()) === 0?'no':'yes';
+      console.log(this.result);
+    }, 4000);
+  }
+  onKey(value: string) {
+    this.disabled = !(value.length > 3);
   }
 
 }
